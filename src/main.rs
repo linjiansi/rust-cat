ec515833535c36bf1dedb3dd72398687855414e3
linjiansi <pmon.ken.1467@gmail.com>
2022-12-21 02:47:39 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::fs::read_to_string;
+use std::env::args;
+
+fn run_cat(path: String) {
+    match read_to_string(path) {
+        Ok(content) => print!("{}", content),
+        Err(reason) => println!("{}", reason),
+    }
+}
+
+fn main() {
+    match args().nth(1) {
+        Some(path) => run_cat(path),
+        None => println!("Please input path"),
+    }
+}
